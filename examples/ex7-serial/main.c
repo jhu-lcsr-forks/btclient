023a109ede683458da65f1732091b291dc532728
th <th@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2005-10-17 20:09:51 +0000

    Added a serial port control example
    git-svn-id: http://web.barrett.com/svn/btclient/trunk@144 3eb8b4e9-0710-d544-b5f2-d3973d62577e

---
+++ examples/ex7-serial/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "btserial.h"
+
+PORT p;
+
+int main(int argc, void **argv){
+    char input[255];
+    char command[20];
+    int err, len;
+
+    if(err = serialOpen(&p, "/dev/ttyS0")){
+        printf("Error opening port: %d", err);
+        exit(0);
+    }
+    serialSetBaud(&p,9600);
+
+    /* Write a string to the port, followed by a carriage return */
+    serialWriteString(&p, "HI\r");
+    while(1){
+	/* Read chars from the port into the input buffer until
+	  the termination character '>' is received or 30 seconds has
+	  elapsed, whichever comes first.
+	 */ 
+	serialReadLine(&p, input, &len, '>', 30000);
+
+    	serialWriteString(&p, "GC\r");
+	serialReadLine(&p, input, &len, '>', 30000);
+    	serialWriteString(&p, "GO\r");
+    }
+    
+    serialClose(&p);
+    exit(0);
+}
