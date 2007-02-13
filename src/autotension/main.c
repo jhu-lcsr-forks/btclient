1089dbc1e9d62b2b1a7bc6905226fe0485a21e6a
bz <bz@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2007-02-13 17:00:10 +0000

    Sinusoidal PID cycling on J3+J4 working
    git-svn-id: http://web.barrett.com/svn/btclient/trunk@325 3eb8b4e9-0710-d544-b5f2-d3973d62577e

--- src/autotension/main.c
+++ src/autotension/main.c
@@ -46,12 +46,23 @@ int done        = FALSE;
 
 wam_struct      *wam;
 btthread        wam_thd;
+int      i3 = 0, i4 = 0;
+long     t = 0;
+double   x3 = 0.0, x4 = 0.0;
+double   A3, A4, P3, P4, S3, S4;
+double   Tt, Ts, m3, m4;
+btPID* jPID;
+via_trj_array *vt_j = NULL;
+int   cycle = 0;
+
+#define pi (3.14159)
 
 /*==============================*
  * PRIVATE Function Prototypes  *
  *==============================*/
 void sigint_handler();
 int mygetch();
+int WAMcallback(struct btwam_struct *wam);
 
 /*==============================*
  * Functions                    *
@@ -61,151 +72,246 @@ int mygetch();
 */
 int main(int argc, char **argv)
 {
-    int     err;
-    int     i;
-    char    robotName[128];
-    char    vect_buf1[250];
-    vect_n* jdest;
-    
-    /* Initialize syslog */
-    openlog("WAM", LOG_CONS | LOG_NDELAY, LOG_USER);
-    atexit((void*)closelog);
+   int      err;
+   int      i;
+   char     robotName[128];
+   char     vect_buf1[250];
+   vect_n*  jdest;
+   char     key[255];
 
-    /* Lead the user through a proper WAM startup */
-    printf("\nMake sure the all WAM power and signal cables are securely");
-    printf("\nfastened, then turn on the main power to WAM and press <Enter>");
-    mygetch();
-    
-    printf("\n\nMake sure all E-STOPs are released, then press Shift-Idle");
-    printf("\non the control pendant. Then press <Enter>");
-    mygetch();
-    
-    printf("\n\nPlace WAM in its home (folded) position, then press <Enter>");
-    mygetch();
+   /* Initialize syslog */
+   openlog("WAM", LOG_CONS | LOG_NDELAY, LOG_USER);
+   atexit((void*)closelog);
 
-#ifndef BTOLDCONFIG
-    err = ReadSystemFromConfig("wam.conf");
-#else //BTOLDCONFIG
-#endif //BTOLDCONFIG
-    /* If the robot name was given on the command line, use it */
-    *robotName = 0; 
-    for(i = 1; i < argc-1; i++){
-        if(!strcmp(argv[i],"-n"))
-            strcpy(robotName, argv[i+1]);
-    }
-    
-    /* Initialize and get a handle to the robot */
-    if(!(wam = OpenWAM("wam.conf", robotName))){
-        printf("\n\nThere was an error while initializing the robot.");
-        printf("\nPlease check /var/log/syslog for details.\n\n");
-        exit(1);
-    }
+   /* Lead the user through a proper WAM startup */
+   printf("\nMake sure the all WAM power and signal cables are securely");
+   printf("\nfastened, then turn on the main power to WAM and press <Enter>");
+   mygetch();
 
-    /* Check and handle any additional command line arguments */
-    for(i = 1; i < argc-1; i++){
-        if(!strcmp(argv[i], "-g")) // If gimbals are being used
-        {
-            initGimbals(wam);
-            useGimbals = TRUE;
-            syslog(LOG_ERR, "Gimbals expected.");
-        }
-    }
+   printf("\n\nMake sure all E-STOPs are released, then press Shift-Idle");
+   printf("\non the control pendant. Then press <Enter>");
+   mygetch();
 
-    /* Register the ctrl-c interrupt handler */
-    signal(SIGINT, sigint_handler);
+   printf("\n\nPlace WAM in its home (folded) position, then press <Enter>");
+   mygetch();
 
-    /* Set the safety limits */
-    setSafetyLimits(2.0, 2.0, 2.0);  // ooh dangerous
-    setProperty(0, 10, TL2, FALSE, 5400); //Eliminate torque faults in silly places
-    setProperty(0, 10, TL1, FALSE, 1800); //Eliminate torque faults in silly places
-    
-    /* Spin off the WAM control thread */
-    wam_thd.period = 0.002;
-    btthread_create(&wam_thd, 90, (void*)WAMControlThread, (void*)wam);
+   err = ReadSystemFromConfig("wam.conf");
 
-    printf("\n\nPlease activate the WAM by pressing Shift-Activate on the");
-    printf("\ncontrol pendant, then press <Enter>");
-    mygetch();
-    
-    // Turn on gravity compensation
-    SetGravityComp(wam, 1.1);
-    
-    const_v3(wam->Cpoint, 0.0, -0.0, 0.05);
-    registerWAMcallback(wam, WAMcallback);
-    
-    // Hold position
-    setmode_bts(wam->active_sc,SCMODE_POS);
-    
-    // Allocate and zero a 7 element vector
-    jdest = new_vn(7);
-    
-    // MoveWAM to zero position
-    MoveSetup(wam, 0.5, 0.5)
-    MoveWAM(wam, jdest);
-    
-    // Set low max torque
-    
-    // MoveWAM to initial tensioning position
-    MoveWAM(wam, const_vn(jdest, -3.14, +2.2, -3.14, +3.5, 0.0, 0.0, 0.0));
-    while(!MoveIsDone(wam))
-            usleep(100000); // Sleep for 0.1s
-    
-    // MoveWAM to present position (stop pushing)
-    MoveWAM(wam, wam->Jpos);
-    while(!MoveIsDone(wam))
-            usleep(100000); // Sleep for 0.1s
-    
-    // Back off J1 by the tension offset (TENSO) of M1
-    
-    // Back off J2 by the tension offset of M2
-    
-    // Back off J4 by the tension offset of M4
-    
-    // Activate solenoids M1, M2, M4
-    setProperty(0, 1, TENSION, FALSE, 1);
-    setProperty(0, 2, TENSION, FALSE, 1);
-    setProperty(0, 4, TENSION, FALSE, 1);
-    
-    // Set tensioning max torque
-    
-    // MoveWAM to apply tension
-    MoveWAM(wam, const_vn(jdest, -3.14, +2.2, <no change>, +3.5, 0.0, 0.0, 0.0));
-    while(!MoveIsDone(wam))
-            usleep(100000); // Sleep for 0.1s
-    
-    // Release solenoids M1, M2, M4
-    setProperty(0, 1, TENSION, FALSE, 0);
-    setProperty(0, 2, TENSION, FALSE, 0);
-    setProperty(0, 4, TENSION, FALSE, 0);
-    
-    // Record tension totals
-    
-    // MoveWAM to present position (stop pushing)
-    MoveWAM(wam, wam->Jpos);
-    while(!MoveIsDone(wam))
-            usleep(100000); // Sleep for 0.1s
-    
-    // Set normal max torque
-    
-    // Work the tension through
-    for(i = 0; i < 8; i++){
-        MoveWAM(wam, const_vn(jdest, +2.5, -1.8, +2.5, -0.7, 0.0, 0.0, 0.0));
-        while(!MoveIsDone(wam))
-            usleep(100000); // Sleep for 0.1s
-        MoveWAM(wam, const_vn(jdest, -2.5, +1.8, -2.5, +3.0, 0.0, 0.0, 0.0));
-        while(!MoveIsDone(wam))
-            usleep(100000); // Sleep for 0.1s
-    }
-    
-    printf("\n\nExiting...\n\n");
-    btthread_stop(&wam_thd); //Kill WAMControlThread
-    exit(1);
+   /* If the robot name was given on the command line, use it */
+   *robotName = 0;
+   for(i = 1; i < argc-1; i++)
+   {
+      if(!strcmp(argv[i],"-n"))
+         strcpy(robotName, argv[i+1]);
+   }
+
+   /* Initialize and get a handle to the robot */
+   if(!(wam = OpenWAM("wam.conf", robotName)))
+   {
+      printf("\n\nThere was an error while initializing the robot.");
+      printf("\nPlease check /var/log/syslog for details.\n\n");
+      exit(1);
+   }
+
+   /* Check and handle any additional command line arguments */
+   for(i = 1; i < argc-1; i++)
+   {
+      if(!strcmp(argv[i], "-g")) // If gimbals are being used
+      {
+         initGimbals(wam);
+         useGimbals = TRUE;
+         syslog(LOG_ERR, "Gimbals expected.");
+      }
+   }
+
+   parseFile("param.txt");
+   Tt = 1.0; // Time between tensioning (hrs)
+   Ts = 0.002; // (s)
+
+   sprintf(key, "J3_Amplitude");
+   parseGetVal(DOUBLE, key, (void*)&A3);
+   
+   sprintf(key, "J4_Amplitude");
+   parseGetVal(DOUBLE, key, (void*)&A4);
+   
+   sprintf(key, "J3_Period");
+   parseGetVal(DOUBLE, key, (void*)&P3);
+   
+   sprintf(key, "J4_Period");
+   parseGetVal(DOUBLE, key, (void*)&P4);
+   
+   sprintf(key, "J3_Shift");
+   parseGetVal(DOUBLE, key, (void*)&S3);
+   
+   sprintf(key, "J4_Shift");
+   parseGetVal(DOUBLE, key, (void*)&S4);
+
+   A3 = A3 * pi / 180;
+   A4 = A4 * pi / 180;
+   S3 = S3 * pi / 180;
+   S4 = S4 * pi / 180;
+   m3 = Ts/P3;
+   m4 = Ts/P4;
+   Tt = Tt * 3600 / Ts;
+
+   printf("\nA3 = %lf, A4 = %lf, P3 = %lf, P4 = %lf, S3 = %lf, S4 = %lf\n",
+      A3, A4, P3, P4, S3, S4);
+      
+   /* Register the ctrl-c interrupt handler */
+   signal(SIGINT, sigint_handler);
+
+   /* Set the safety limits */
+   setSafetyLimits(2.0, 2.0, 2.0);  // ooh dangerous
+   setProperty(0, 10, TL2, FALSE, 5400); //Eliminate torque faults in silly places
+   setProperty(0, 10, TL1, FALSE, 1800); //Eliminate torque faults in silly places
+
+   const_v3(wam->Cpoint, 0.0, 0.0, 0.0);
+
+   /* Create a new trajectory */
+   vt_j = new_vta(wam->dof,50);
+   register_vta(&(wam->Jsc),vt_j);
+   
+   jPID = (btPID*)btmalloc(wam->dof * sizeof(btPID));
+   for(i = 0; i < wam->dof; i++){
+      init_btPID(&jPID[i]);
+      setdt_btPID(&jPID[i], Ts);
+      setgains_btPID(&jPID[i], 1000.0, 5.0, 1.0);
+      setsaturation_btPID(&jPID[i], 0.0);
+   }
+   
+   /* Spin off the WAM control thread */
+   wam_thd.period = Ts;
+   registerWAMcallback(wam, WAMcallback);
+   btthread_create(&wam_thd, 90, (void*)WAMControlThread, (void*)wam);
+
+   printf("\n\nPlease activate the WAM by pressing Shift-Activate on the");
+   printf("\ncontrol pendant, then press <Enter>");
+   mygetch();
+
+   // Turn on gravity compensation
+   SetGravityComp(wam, 1.0);
+
+   // Hold position
+   setmode_bts(wam->active_sc,SCMODE_POS);
+
+   // Allocate and zero a 7 element vector
+   jdest = new_vn(7);
+
+   MoveWAM(wam, const_vn(jdest, 0.0, 0.0, A3*sin(x3)+S3, A4*sin(x4)+S4, 0.0, 0.0, 0.0));
+   while(!MoveIsDone(wam)) 
+      usleep(1000);
+
+   sety_btPID(&jPID[2], wam->Jpos->q[2]);
+   sety_btPID(&jPID[3], wam->Jpos->q[3]);
+   start_btPID(&jPID[2]);
+   start_btPID(&jPID[3]);
+   cycle = 1;
+   while(1){
+      
+      
+      
+      if(t > Tt)
+      {
+         t = 0;
+         //Tension(M4);
+         //Move to start (0, -2pi, A3*sin(x3)+S3, A4*sin(x4)+S4);
+      }
+   }
+   // MoveWAM to zero position
+   //MoveSetup(wam, 0.5, 0.5);
+   //MoveWAM(wam, jdest);
+
+   //mygetch();
+#if 0
+   // Set low max torque
+
+   // MoveWAM to initial tensioning position
+   MoveWAM(wam, const_vn(jdest, -3.14, +2.2, -3.14, +3.5, 0.0, 0.0, 0.0));
+   while(!MoveIsDone(wam))
+      usleep(100000); // Sleep for 0.1s
+
+   // MoveWAM to present position (stop pushing)
+   MoveWAM(wam, wam->Jpos);
+   while(!MoveIsDone(wam))
+      usleep(100000); // Sleep for 0.1s
+
+   // Back off J1 by the tension offset (TENSO) of M1
+
+   // Back off J2 by the tension offset of M2
+
+   // Back off J4 by the tension offset of M4
+
+   // Activate solenoids M1, M2, M4
+   setProperty(0, 1, TENSION, FALSE, 1);
+   setProperty(0, 2, TENSION, FALSE, 1);
+   setProperty(0, 4, TENSION, FALSE, 1);
+
+   // Set tensioning max torque
+
+   // MoveWAM to apply tension
+   MoveWAM(wam, const_vn(jdest, -3.14, +2.2, <no change>, +3.5, 0.0, 0.0, 0.0));
+   while(!MoveIsDone(wam))
+      usleep(100000); // Sleep for 0.1s
+
+   // Release solenoids M1, M2, M4
+   setProperty(0, 1, TENSION, FALSE, 0);
+   setProperty(0, 2, TENSION, FALSE, 0);
+   setProperty(0, 4, TENSION, FALSE, 0);
+
+   // Record tension totals
+
+   // MoveWAM to present position (stop pushing)
+   MoveWAM(wam, wam->Jpos);
+   while(!MoveIsDone(wam))
+      usleep(100000); // Sleep for 0.1s
+
+   // Set normal max torque
+
+   // Work the tension through
+   for(i = 0; i < 8; i++)
+   {
+      MoveWAM(wam, const_vn(jdest, +2.5, -1.8, +2.5, -0.7, 0.0, 0.0, 0.0));
+      while(!MoveIsDone(wam))
+         usleep(100000); // Sleep for 0.1s
+      MoveWAM(wam, const_vn(jdest, -2.5, +1.8, -2.5, +3.0, 0.0, 0.0, 0.0));
+      while(!MoveIsDone(wam))
+         usleep(100000); // Sleep for 0.1s
+   }
+
+#endif
+
+   printf("\n\nExiting...\n\n");
+   btthread_stop(&wam_thd); //Kill WAMControlThread
+   exit(1);
 }
 
 int WAMcallback(struct btwam_struct *wam)
 {
-
-    return 0;
+   btreal j3, j4;
+   
+   if(cycle){
+      i3++;
+      if(i3*Ts > P3)
+         i3 = 0;
+      x3 = 2 * pi * i3 * m3;
+      
+      i4++;
+      if(i4*Ts > P4)
+         i4 = 0;
+      x4 = 2 * pi * i4 * m4;
+      
+      
+      j3 = A3*sin(x3)+S3;
+      j4 = A4*sin(x4)+S4;
+      //printf("\rj3 = %lf, j4 = %lf", j3, j4);
+      
+      wam->Jtrq->q[2] = eval_btPID(&jPID[2], wam->Jpos->q[2], j3, Ts);
+      wam->Jtrq->q[3] = eval_btPID(&jPID[3], wam->Jpos->q[3], j4, Ts);
+      
+      t++;
+      //cycle = 0;
+   }
+   return 0;
 }
 
 /** Traps the Ctrl-C signal.
@@ -213,31 +319,92 @@ int WAMcallback(struct btwam_struct *wam)
 */
 void sigint_handler()
 {
-    done = TRUE;
+   done = TRUE;
+   
+   printf("\n\nExiting...\n\n");
+   btthread_stop(&wam_thd); //Kill WAMControlThread
+   exit(1);
 }
 
-/** Return immediately after any key is pressed. 
+/** Return immediately after any key is pressed.
     You do not have to wait till the enter key is hit .
 */
 int mygetch()
 {
-    /* Handles to old and new termios structures */
-    struct termios oldt, newt;
-    int ch;
+   /* Handles to old and new termios structures */
+   struct termios oldt, newt;
+   int ch;
 
-    /* store current termios to restore back later */
-    tcgetattr( STDIN_FILENO, &oldt );
-    newt = oldt;
+   /* store current termios to restore back later */
+   tcgetattr( STDIN_FILENO, &oldt );
+   newt = oldt;
 
-    /* set to canonical. turn off echo */
-    newt.c_lflag &= ~( ICANON | ECHO );
-    tcsetattr( STDIN_FILENO, TCSANOW, &newt );
+   /* set to canonical. turn off echo */
+   newt.c_lflag &= ~( ICANON | ECHO );
+   tcsetattr( STDIN_FILENO, TCSANOW, &newt );
 
-    /* Read the character */
-    ch = getchar();
+   /* Read the character */
+   ch = getchar();
 
-    /* Reset terminal */
-    tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
-    return ch;
+   /* Reset terminal */
+   tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
+   return ch;
 }
 
+#if 0
+A*sin(Px)+S;
+A = Amplitude (deg)
+    P = Period (s)
+        S = Shift (deg)
+
+            Ts = 0.002 (s)
+                 Tt = 1.0 (hrs)
+
+                      INIT
+                      int i3 = i4 = 0;
+long t = 0;
+double x3 = x4 = 0.0;
+Start in home position
+Hold Position
+S3 = S3 * pi / 180;
+S4 = S4 * pi / 180;
+m3 = Ts/P3;
+m4 = Ts/P4;
+Tt = Tt * 3600 / Ts;
+
+Move to start (0, -2pi, A3*sin(x3)+S3, A4*sin(x4)+S4);
+
+RUN
+i3++;
+if(i3*Ts > P3)
+   i3 = 0;
+x3 = 2pi * i3 * m3;
+
+i4++;
+if(i4*Ts > P4)
+   i4 = 0;
+x4 = 2pi * i4 * m4;
+
+set_yref(J3pid, A3*sin(x3)+S3);
+set_yref(J4pid, A4*sin(x4)+S4);
+
+t++;
+if(t > Tt)
+{
+   t = 0;
+   Tension(M4);
+   Move to start (0, -2pi, A3*sin(x3)+S3, A4*sin(x4)+S4);
+}
+
+Tension(M){
+   M1(-J1, 0, 0, pi/2);
+   M2(0, J2, -J3, pi/4);
+   M3(0, -J2, -J3, pi/4);
+   M4(0, 0, 0, -J4);
+
+   Move to Mx();
+   Activate tensioner
+   Rotate until engaged
+   Record
+}
+#endif
