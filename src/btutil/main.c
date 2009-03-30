dc5e282db3f0b5ab0e0147719ed0017f4d6ac39e
cd <cd@3eb8b4e9-0710-d544-b5f2-d3973d62577e>
2009-03-30 16:07:53 +0000

    Fixed some source formatting inconsistencies
    git-svn-id: http://web.barrett.com/svn/btclient/trunk@556 3eb8b4e9-0710-d544-b5f2-d3973d62577e

--- src/btutil/main.c
+++ src/btutil/main.c
@@ -970,30 +970,30 @@ void changeID(int oldID, int newID, int role)
 {
    int _LOCK, _SAVE;
    
-   setPropertySlow(0, oldID, 5, 0, 0); /* RESET back to Monitor */ 
-   usleep(2000000); /* Wait 2s */ 
-    
-   if(oldID == 10){ /* Safety puck's LOCK/SAVE commands are different */ 
-      _LOCK = 13; _SAVE = 30; 
-   }else{ 
-      _LOCK = 8; _SAVE = 9; 
-   } 
-   /* Unlock the ID/ROLE for writing */ 
-   setPropertySlow(0, oldID, _LOCK, 0, 18384);  
-   setPropertySlow(0, oldID, _LOCK, 0, 23);  
-   setPropertySlow(0, oldID, _LOCK, 0, 3145);  
-   setPropertySlow(0, oldID, _LOCK, 0, 1024);  
-   setPropertySlow(0, oldID, _LOCK, 0, 1);  
-    
+   setPropertySlow(0, oldID, 5, 0, 0); /* RESET back to Monitor */
+   usleep(2000000); /* Wait 2s */
+   
+   if(oldID == 10){ /* Safety puck's LOCK/SAVE commands are different */
+      _LOCK = 13; _SAVE = 30;
+   }else{
+      _LOCK = 8; _SAVE = 9;
+   }
+   /* Unlock the ID/ROLE for writing */
+   setPropertySlow(0, oldID, _LOCK, 0, 18384); 
+   setPropertySlow(0, oldID, _LOCK, 0, 23); 
+   setPropertySlow(0, oldID, _LOCK, 0, 3145); 
+   setPropertySlow(0, oldID, _LOCK, 0, 1024); 
+   setPropertySlow(0, oldID, _LOCK, 0, 1); 
+   
    /* Set the ROLE */
    if(role >= 0){
       setPropertySlow(0, oldID, 1, 0, role); 
-      setPropertySlow(0, oldID, _SAVE, 0, 1); usleep(2000000); 
- 	} 
- 	setPropertySlow(0, oldID, 3, 0, newID); /* Set the new ID */ 
- 	setPropertySlow(0, oldID, _SAVE, 0, 3); usleep(2000000); /* Save the new values to EEPROM */ 
- 	 
- 	/* Reset to load the new ID */
+      setPropertySlow(0, oldID, _SAVE, 0, 1); usleep(2000000);
+   }
+   setPropertySlow(0, oldID, 3, 0, newID); /* Set the new ID */
+   setPropertySlow(0, oldID, _SAVE, 0, 3); usleep(2000000); /* Save the new values to EEPROM */
+   
+   /* Reset to load the new ID */
    setPropertySlow(0, oldID, 5, 0, STATUS_RESET);
 }
 
@@ -1002,8 +1002,8 @@ void setMofst(int newID)
    long dat, vers;
    int dummy, i, samples = 16;
    
-	long	max, min;
-	double	sumX, sumX2, mean, stdev;
+   long max, min;
+   double sumX, sumX2, mean, stdev;
    int err = 0;
    
    wakePuck(0,newID);
@@ -1175,12 +1175,12 @@ void handleMenu(int argc, char **argv)
    while(*c == '-') c++;
    *c = toupper(*c);
    
-   switch(*c) { /* Only getBusStatus if not BHandDL, PuckDL, PuckID, or Quit */ 
- 	case 'B': case 'D': case 'I': case 'Q': 
- 	break; 
- 	default: 
- 	   getBusStatus(0, status); 
- 	} 
+   switch(*c) { /* Only getBusStatus if not BHandDL, PuckDL, PuckID, or Quit */
+   case 'B': case 'D': case 'I': case 'Q':
+   break;
+   default:
+      getBusStatus(0, status);
+   }
    
    switch(*c) {
    case 'H':
